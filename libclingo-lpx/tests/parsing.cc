d4bfaa878f7188c421513de030e33c32efa4bcf8
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2022-09-18 15:36:06 +0200

    update to catch 3.1

--- libclingo-lpx/tests/parsing.cc
+++ libclingo-lpx/tests/parsing.cc
@@ -1,5 +1,5 @@
 #include "../src/parsing.hh"
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 #include <sstream>
 
--- libclingo-lpx/tests/solving.cc
+++ libclingo-lpx/tests/solving.cc
@@ -1,7 +1,7 @@
 #include <parsing.hh>
 #include <solving.hh>
 
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 namespace {
 
