abdb6f32f134acabebbc2adc517a20ba5d9a325b
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2023-01-22 00:40:33 +0100

    add find module for flint library

---
+++ cmake/FindFLINT.cmake
@@ -0,0 +1,53 @@
+set(gmp_library_search_hints)
+set(flint_include_search_hints)
+set(flint_library_search_hints)
+
+if(DEFINED GMP_DIR)
+    list(APPEND gmp_library_search_hints ${GMP_DIR})
+    list(APPEND gmp_library_search_hints ${GMP_DIR}/lib)
+endif()
+
+if(DEFINED ENV{GMP_DIR})
+    list(APPEND gmp_library_search_hints $ENV{FLINT_DIR})
+    list(APPEND gmp_library_search_hints $ENV{FLINT_DIR}/lib)
+endif()
+
+if(DEFINED FLINT_DIR)
+    list(APPEND flint_library_search_hints ${FLINT_DIR})
+    list(APPEND flint_library_search_hints ${FLINT_DIR}/lib)
+    list(APPEND flint_include_search_hints ${FLINT_DIR})
+    list(APPEND flint_include_search_hints ${FLINT_DIR}/include)
+endif()
+
+if(DEFINED ENV{FLINT_DIR})
+    list(APPEND flint_library_search_hints $ENV{FLINT_DIR})
+    list(APPEND flint_library_search_hints $ENV{FLINT_DIR}/lib)
+    list(APPEND flint_include_search_hints $ENV{FLINT_DIR})
+    list(APPEND flint_include_search_hints $ENV{FLINT_DIR}/include)
+endif()
+
+find_library(
+    GMP_LIBRARY 
+    NAMES gmp
+    HINTS ${gmp_library_search_hints})
+
+find_path(
+    FLINT_INCLUDE 
+    NAMES flint/flint.h flint.h
+    HINTS ${flint_include_search_hints})
+
+find_library(
+    FLINT_LIBRARY 
+    NAMES flint 
+    HINTS ${flint_library_search_hints})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(FLINT DEFAULT_MSG GMP_LIBRARY FLINT_INCLUDE FLINT_LIBRARY)
+mark_as_advanced(GMP_LIBRARY FLINT_INCLUDE FLINT_LIBRARY)
+
+set(FLINT_LIBRARIES ${FLINT_LIBRARY} ${GMP_LIBRARY})
+set(FLINT_INCLUDE_DIRS ${FLINT_INCLUDE})
+
+add_library(FLINT::FLINT INTERFACE IMPORTED)
+set_property(TARGET FLINT::FLINT PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${FLINT_INCLUDE_DIRS}")
+set_property(TARGET FLINT::FLINT PROPERTY INTERFACE_LINK_LIBRARIES "${FLINT_LIBRARIES}")
