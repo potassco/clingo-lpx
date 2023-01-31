d0915f609fd7ad5768e934c92e7b1a08ac8b2778
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2023-01-31 19:44:03 +0100

    complete imath implementation (still buggy)

--- libclingo-lpx/src/number_imath.hh
+++ libclingo-lpx/src/number_imath.hh
@@ -12,8 +12,6 @@
 #include <memory>
 #include <iostream>
 
-// TODO: it looks like imath also supports aliasing parameters
-
 constexpr int BASE = 10;
 
 using fixed_int = mp_small;
@@ -227,7 +225,7 @@ inline Integer::Integer(std::string const &val, int radix)
 
 inline Integer::Integer(Integer const &a)
 : Integer() {
-    mp_handle_error_(mp_int_copy(&num_, &a.num_));
+    mp_handle_error_(mp_int_copy(&a.num_, &num_));
 }
 
 inline Integer::Integer(Integer &&a) noexcept
@@ -236,7 +234,7 @@ inline Integer::Integer(Integer &&a) noexcept
 }
 
 inline Integer &Integer::operator=(Integer const &a) {
-    mp_handle_error_(mp_int_init_copy(&num_, &a.num_));
+    mp_handle_error_(mp_int_copy(&a.num_, &num_));
     return *this;
 }
 
@@ -293,11 +291,13 @@ inline mpz_t &Integer::impl() const {
 }
 
 inline Integer &operator+=(Integer &a, fixed_int b) {
-    return a = a + b;
+    mp_handle_error_(mp_int_add_value(&a.num_, b, &a.num_));
+    return a;
 }
 
 inline Integer &operator+=(Integer &a, Integer const &b) {
-    return a = a + b;
+    mp_handle_error_(mp_int_add(&a.num_, &b.num_, &a.num_));
+    return a;
 }
 
 // subtraction
@@ -328,11 +328,13 @@ inline Integer &operator+=(Integer &a, Integer const &b) {
 }
 
 inline Integer &operator-=(Integer &a, fixed_int b) {
-    return a = a + b;
+    mp_handle_error_(mp_int_sub_value(&a.num_, b, &a.num_));
+    return a;
 }
 
 inline Integer &operator-=(Integer &a, Integer const &b) {
-    return a = a + b;
+    mp_handle_error_(mp_int_sub(&a.num_, &b.num_, &a.num_));
+    return a;
 }
 
 // multiplication
@@ -358,11 +360,13 @@ inline Integer &operator-=(Integer &a, Integer const &b) {
 }
 
 inline Integer &operator*=(Integer &a, fixed_int b) {
-    return a = a * b;
+    mp_handle_error_(mp_int_mul_value(&a.num_, b, &a.num_));
+    return a;
 }
 
 inline Integer &operator*=(Integer &a, Integer const &b) {
-    return a = a * b;
+    mp_handle_error_(mp_int_mul(&a.num_, &b.num_, &a.num_));
+    return a;
 }
 
 // less than
@@ -430,7 +434,7 @@ inline Integer &operator*=(Integer &a, Integer const &b) {
 
 inline std::ostream &operator<<(std::ostream &out, Integer const &a) {
     auto len = mp_int_string_len(&a.num_, BASE);
-    std::unique_ptr<char[]> buf{new char[len]}; // NOLINT
+    std::unique_ptr<char[]> buf{std::make_unique<char[]>(len)}; // NOLINT
     mp_handle_error_(mp_int_to_string(&a.num_, BASE, buf.get(), len));
     out << buf.get();
     return out;
@@ -450,12 +454,11 @@ inline std::ostream &operator<<(std::ostream &out, Integer const &a) {
 
 [[nodiscard]] inline std::tuple<Integer, Integer, Integer> gcd(Integer const &a, Integer const &b) {
     Integer g;
-    Integer r;
     Integer ga;
     Integer gb;
     mp_int_gcd(&a.num_, &b.num_, &g.num_);
-    mp_int_div(&a.num_, &g.num_, &ga.num_, &r.num_);
-    mp_int_div(&b.num_, &g.num_, &gb.num_, &r.num_);
+    mp_int_div(&a.num_, &g.num_, &ga.num_, nullptr);
+    mp_int_div(&b.num_, &g.num_, &gb.num_, nullptr);
     return {g, ga, gb};
 }
 
@@ -481,7 +484,7 @@ inline Rational::Rational(std::string const &val, int radix)
 
 inline Rational::Rational(Rational const &a)
 : Rational() {
-    mp_handle_error_(mp_rat_copy(&num_, &a.num_));
+    mp_handle_error_(mp_rat_copy(&a.num_, &num_));
 }
 
 inline Rational::Rational(Integer num, Integer den)
@@ -543,27 +546,22 @@ inline void Rational::swap(Rational &x) noexcept {
 inline void Rational::canonicalize() {
     mp_handle_error_(mp_rat_reduce(&num_));
 }
+
 // addition
 
 [[nodiscard]] inline Rational operator+(Rational const &a, fixed_int b) {
-#if __FLINT_RELEASE >= 20600
-    Rational c;
-    fmpq_add_si(&c.num_, &a.num_, &b.num_);
-    return c;
-#else
-    return a + Rational{b};
-#endif
+    return a + Integer{b};
 }
 
 [[nodiscard]] inline Rational operator+(Rational const &a, Integer const &b) {
     Rational c;
-    fmpq_add_fmpz(&c.num_, &a.num_, &b.impl());
+    mp_handle_error_(mp_rat_add_int(&a.num_, &b.impl(), &c.num_));
     return c;
 }
 
 [[nodiscard]] inline Rational operator+(Rational const &a, Rational const &b) {
     Rational c;
-    fmpq_add(&c.num_, &a.num_, &b.num_);
+    mp_handle_error_(mp_rat_add(&a.num_, &b.num_, &c.num_));
     return c;
 }
 
@@ -580,17 +578,16 @@ inline void Rational::canonicalize() {
 }
 
 inline Rational &operator+=(Rational &a, fixed_int b) {
-    fmpq_add_si(&a.num_, &a.num_, b);
-    return a;
+    return a += Integer{b};
 }
 
 inline Rational &operator+=(Rational &a, Integer const &b) {
-    fmpq_add_fmpz(&a.num_, &a.num_, &b.impl());
+    mp_handle_error_(mp_rat_add_int(&a.num_, &b.impl(), &a.num_));
     return a;
 }
 
 inline Rational &operator+=(Rational &a, Rational const &b) {
-    fmpq_add(&a.num_, &a.num_, &b.num_);
+    mp_handle_error_(mp_rat_add(&a.num_, &b.num_, &a.num_));
     return a;
 }
 
@@ -602,24 +599,18 @@ inline Rational &operator+=(Rational &a, Rational const &b) {
 }
 
 [[nodiscard]] inline Rational operator-(Rational const &a, fixed_int b) {
-#if __FLINT_RELEASE >= 20600
-    Rational c;
-    fmpq_sub_si(&c.num_, &a.num_, &b.num_);
-    return c;
-#else
-    return a - Rational{b};
-#endif
+    return a - Integer{b};
 }
 
 [[nodiscard]] inline Rational operator-(Rational const &a, Integer const &b) {
     Rational c;
-    fmpq_sub_fmpz(&c.num_, &a.num_, &b.impl());
+    mp_handle_error_(mp_rat_sub_int(&a.num_, &b.impl(), &c.num_));
     return c;
 }
 
 [[nodiscard]] inline Rational operator-(Rational const &a, Rational const &b) {
     Rational c;
-    fmpq_sub(&c.num_, &a.num_, &b.num_);
+    mp_handle_error_(mp_rat_sub(&a.num_, &b.num_, &c.num_));
     return c;
 }
 
@@ -636,41 +627,34 @@ inline Rational &operator+=(Rational &a, Rational const &b) {
 }
 
 inline Rational &operator-=(Rational &a, fixed_int b) {
-    fmpq_sub_si(&a.num_, &a.num_, b);
-    return a;
+    return a -= Integer{b};
 }
 
 inline Rational &operator-=(Rational &a, Integer const &b) {
-    fmpq_sub_fmpz(&a.num_, &a.num_, &b.impl());
+    mp_handle_error_(mp_rat_sub_int(&a.num_, &b.impl(), &a.num_));
     return a;
 }
 
 inline Rational &operator-=(Rational &a, Rational const &b) {
-    fmpq_sub(&a.num_, &a.num_, &b.num_);
+    mp_handle_error_(mp_rat_sub(&a.num_, &b.num_, &a.num_));
     return a;
 }
 
 // multiplication
 
 [[nodiscard]] inline Rational operator*(Rational const &a, fixed_int b) {
-#if __FLINT_RELEASE >= 20600
-    Rational c;
-    fmpq_mul_si(&c.num_, &a.num_, &b.num_);
-    return c;
-#else
-    return a * Rational{b};
-#endif
+    return a * Integer{b};
 }
 
 [[nodiscard]] inline Rational operator*(Rational const &a, Integer const &b) {
     Rational c;
-    fmpq_mul_fmpz(&c.num_, &a.num_, &b.impl());
+    mp_handle_error_(mp_rat_mul_int(&a.num_, &b.impl(), &c.num_));
     return c;
 }
 
 [[nodiscard]] inline Rational operator*(Rational const &a, Rational const &b) {
     Rational c;
-    fmpq_mul(&c.num_, &a.num_, &b.num_);
+    mp_handle_error_(mp_rat_mul(&a.num_, &b.num_, &c.num_));
     return c;
 }
 
@@ -687,45 +671,34 @@ inline Rational &operator-=(Rational &a, Rational const &b) {
 }
 
 inline Rational &operator*=(Rational &a, fixed_int b) {
-#if __FLINT_RELEASE >= 20600
-    fmpq_mul_si(&a.num_, &a.num_, b);
-    return a;
-#else
-    return a *= Rational{b};
-#endif
+    return a *= Integer{b};
 }
 
 inline Rational &operator*=(Rational &a, Integer const &b) {
-    fmpq_mul_fmpz(&a.num_, &a.num_, &b.impl());
+    mp_handle_error_(mp_rat_mul_int(&a.num_, &b.impl(), &a.num_));
     return a;
 }
 
 inline Rational &operator*=(Rational &a, Rational const &b) {
-    fmpq_mul(&a.num_, &a.num_, &b.num_);
+    mp_handle_error_(mp_rat_mul(&a.num_, &b.num_, &a.num_));
     return a;
 }
 
 // division
 
 [[nodiscard]] inline Rational operator/(Rational const &a, fixed_int b) {
-#if __FLINT_RELEASE >= 20600
-    Rational c;
-    fmpq_div_si(&c.num_, &a.num_, &b.num_);
-    return c;
-#else
-    return a / Rational{b};
-#endif
+    return a / Integer{b};
 }
 
 [[nodiscard]] inline Rational operator/(Rational const &a, Integer const &b) {
     Rational c;
-    fmpq_div_fmpz(&c.num_, &a.num_, &b.impl());
+    mp_handle_error_(mp_rat_div_int(&a.num_, &b.impl(), &c.num_));
     return c;
 }
 
 [[nodiscard]] inline Rational operator/(Rational const &a, Rational const &b) {
     Rational c;
-    fmpq_div(&c.num_, &a.num_, &b.num_);
+    mp_handle_error_(mp_rat_div(&a.num_, &b.num_, &c.num_));
     return c;
 }
 
@@ -742,21 +715,16 @@ inline Rational &operator*=(Rational &a, Rational const &b) {
 }
 
 inline Rational &operator/=(Rational &a, fixed_int b) {
-#if __FLINT_RELEASE >= 20600
-    fmpq_div_si(&a.num_, &a.num_, b);
-    return a;
-#else
-    return a /= Rational{b};
-#endif
+    return a /= Integer{b};
 }
 
 inline Rational &operator/=(Rational &a, Integer const &b) {
-    fmpq_div_fmpz(&a.num_, &a.num_, &b.impl());
+    mp_handle_error_(mp_rat_div_int(&a.num_, &b.impl(), &a.num_));
     return a;
 }
 
 inline Rational &operator/=(Rational &a, Rational const &b) {
-    fmpq_div(&a.num_, &a.num_, &b.num_);
+    mp_handle_error_(mp_rat_div(&a.num_, &b.num_, &a.num_));
     return a;
 }
 
@@ -819,252 +787,62 @@ inline Rational &operator/=(Rational &a, Rational const &b) {
 // equal to
 
 [[nodiscard]] inline bool operator==(Rational const &a, fixed_int b) {
-#if __FLINT_RELEASE >= 20600
-        return fmpq_equal_si(&a.num_, b) != 0;
-#else
-        return a == Rational{b};
-#endif
+    return compare(a, b) == 0;
 }
 
 [[nodiscard]] inline bool operator==(Rational const &a, Integer const &b) {
-#if __FLINT_RELEASE >= 20600
-        return fmpq_equal_fmpz(&a.num_, b) != 0;
-#else
-        return a == Rational{b, Integer{1}};
-#endif
+    return compare(a, b) == 0;
 }
 
 [[nodiscard]] inline bool operator==(Rational const &a, Rational const &b) {
-        return fmpq_equal(&a.num_, &b.num_) != 0;
+    return compare(a, b) == 0;
 }
 
 // not equal to
 
 [[nodiscard]] inline bool operator!=(Rational const &a, fixed_int b) {
-    return !(a == b);
+    return compare(a, b) != 0;
 }
 
 [[nodiscard]] inline bool operator!=(Rational const &a, Integer const &b) {
-    return !(a == b);
+    return compare(a, b) != 0;
 }
 
 [[nodiscard]] inline bool operator!=(Rational const &a, Rational const &b) {
-    return !(a == b);
+    return compare(a, b) != 0;
 }
 
 // printing
 
 inline std::ostream &operator<<(std::ostream &out, Rational const &a) {
-    std::unique_ptr<char, decltype(flint_free) *> buf{fmpq_get_str(nullptr, BASE, &a.num_), flint_free};
-    if (buf == nullptr) {
-        throw std::bad_alloc();
+    if (mp_int_compare_value(mp_rat_denom_ref(&a.num_), 1) == 0) {
+        auto len = mp_int_string_len(mp_rat_numer_ref(&a.num_), BASE);
+        auto buf = std::make_unique<char[]>(len); // NOLINT
+        mp_handle_error_(mp_int_to_string(mp_rat_numer_ref(&a.num_), BASE, buf.get(), len));
+        out << buf.get();
     }
-    out << buf.get();
+    else {
+        auto len = mp_rat_string_len(&a.num_, BASE);
+        auto buf = std::make_unique<char[]>(len); // NOLINT
+        mp_handle_error_(mp_rat_to_string(&a.num_, BASE, buf.get(), len));
+        out << buf.get();
+    }
     return out;
 }
 
 // comparison
 
 [[nodiscard]] inline int compare(Rational const &a, fixed_int b) {
-#if __FLINT_RELEASE >= 20600
-        return fmpq_cmp_si(&a.num_, b);
-#else
-        return compare(a, Rational{b});
-#endif
+    return mp_rat_compare_value(&a.num_, b, 1);
 }
 
 [[nodiscard]] inline int compare(Rational const &a, Integer const &b) {
-#if __FLINT_RELEASE >= 20600
-        return fmpq_cmp_fmpz(&a.num_, &b.impl());
-#else
-        return compare(a, Rational{b, 1});
-#endif
+    // Note: this is not good.
+    return compare(a, Rational{b, Integer{1}});
 }
 
 [[nodiscard]] inline int compare(Rational const &a, Rational const &b) {
-        return fmpq_cmp(&a.num_, &b.num_);
+    return mp_rat_compare(&a.num_, &b.num_);
 }
-/*
-
-class Rational {
-public:
-    Rational() { // NOLINT
-        mp_handle_error_(mp_rat_init(&num_));
-    }
-    Rational(mp_small val)
-    : Rational() {
-        mp_handle_error_(mp_rat_set_value(&num_, val, 1));
-    }
-    Rational(char const *val, mp_size radix)
-    : Rational() {
-        mp_handle_error_(mp_rat_read_string(&num_, radix, val));
-    }
-    Rational(std::string const &val, mp_size radix)
-    : Rational(val.c_str(), radix) {
-    }
-    Rational(Rational const &a)
-    : Rational() {
-        mp_handle_error_(mp_rat_copy(&a.num_, &num_));
-    }
-    Rational(Rational &&a) noexcept
-    : Rational() {
-        swap(a);
-    }
-    Rational &operator=(Rational const &a) {
-        mp_handle_error_(mp_rat_copy(&a.num_, &num_));
-        return *this;
-    }
-    Rational &operator=(Rational &&a) noexcept {
-        swap(a);
-        return *this;
-    }
-    ~Rational() {
-        mp_rat_clear(&num_);
-    }
-
-    void swap(Rational &x) {
-        mp_int_swap(mp_rat_numer_ref(&num_), mp_rat_numer_ref(&x.num_));
-        mp_int_swap(mp_rat_denom_ref(&num_), mp_rat_denom_ref(&x.num_));
-    }
-
-    void canonicalize() {
-        mp_handle_error_(mp_rat_reduce(&num_));
-    }
-
-    friend Rational operator*(Rational const &a, Rational const &b) {
-        Rational c;
-        mp_handle_error_(mp_rat_mul(&a.num_, &b.num_, &c.num_));
-        return c;
-    }
-
-    friend Rational &operator*=(Rational &a, Rational const &b) {
-        Rational c;
-        mp_handle_error_(mp_rat_mul(&a.num_, &b.num_, &c.num_));
-        c.swap(a);
-        return a;
-    }
-
-    friend Rational operator/(Rational const &a, Rational const &b) {
-        Rational c;
-        mp_handle_error_(mp_rat_div(&a.num_, &b.num_, &c.num_));
-        return c;
-    }
-
-    friend Rational &operator/=(Rational &a, Rational const &b) {
-        mp_handle_error_(mp_rat_div(&a.num_, &b.num_, &a.num_));
-        return a;
-    }
-
-    friend Rational operator+(Rational const &a, Rational const &b) {
-        Rational c;
-        mp_handle_error_(mp_rat_add(&a.num_, &b.num_, &c.num_));
-        return c;
-    }
-    friend Rational &operator+=(Rational &a, Rational const &b) {
-        Rational c;
-        mp_handle_error_(mp_rat_add(&a.num_, &b.num_, &c.num_));
-        c.swap(a);
-        return a;
-    }
-
-    friend Rational operator-(Rational const &a) {
-        Rational b;
-        mp_handle_error_(mp_rat_neg(&a.num_, &b.num_));
-        return b;
-    }
-    friend Rational operator-(Rational const &a, Rational const &b) {
-        Rational c;
-        mp_handle_error_(mp_rat_sub(&a.num_, &b.num_, &c.num_));
-        return c;
-    }
-    friend Rational &operator-=(Rational &a, Rational const &b) {
-        Rational c;
-        mp_handle_error_(mp_rat_sub(&a.num_, &b.num_, &c.num_));
-        c.swap(a);
-        return a;
-    }
-
-    friend bool operator<(Rational const &a, Rational const &b) {
-        return mp_rat_compare(&a.num_, &b.num_) < 0;
-    }
-    friend bool operator<=(Rational const &a, Rational const &b) {
-        return mp_rat_compare(&a.num_, &b.num_) <= 0;
-    }
-    friend bool operator>(Rational const &a, Rational const &b) {
-        return mp_rat_compare(&a.num_, &b.num_) > 0;
-    }
-    friend bool operator>=(Rational const &a, Rational const &b) {
-        return mp_rat_compare(&a.num_, &b.num_) >= 0;
-    }
-    friend bool operator==(Rational const &a, Rational const &b) {
-        return mp_rat_compare(&a.num_, &b.num_) == 0;
-    }
-    friend bool operator!=(Rational const &a, Rational const &b) {
-        return mp_rat_compare(&a.num_, &b.num_) != 0;
-    }
-    friend bool operator<(Rational const &a, mp_small b) {
-        return mp_rat_compare_value(&a.num_, b, 1) < 0;
-    }
-    friend bool operator<=(Rational const &a, mp_small b) {
-        return mp_rat_compare_value(&a.num_, b, 1) <= 0;
-    }
-    friend bool operator>(Rational const &a, mp_small b) {
-        return mp_rat_compare_value(&a.num_, b, 1) > 0;
-    }
-    friend bool operator>=(Rational const &a, mp_small b) {
-        return mp_rat_compare_value(&a.num_, b, 1) >= 0;
-    }
-    friend bool operator==(Rational const &a, mp_small b) {
-        return mp_rat_compare_value(&a.num_, b, 1) == 0;
-    }
-    friend bool operator!=(Rational const &a, mp_small b) {
-        return mp_rat_compare_value(&a.num_, b, 1) != 0;
-    }
-
-    friend int compare(Rational const &a, Rational const &b) {
-        return mp_rat_compare(&a.num_, &b.num_);
-    }
-
-    friend std::ostream &operator<<(std::ostream &out, Rational const &a) {
-        constexpr int radix = 10;
-        if (mp_int_compare_value(mp_rat_denom_ref(&a.num_), 1) == 0) {
-            auto len = mp_int_string_len(mp_rat_numer_ref(&a.num_), radix);
-            auto buf = std::make_unique<char[]>(len); // NOLINT
-            mp_handle_error_(mp_int_to_string(mp_rat_numer_ref(&a.num_), radix, buf.get(), len));
-            out << buf.get();
-        }
-        else {
-            auto len = mp_rat_string_len(&a.num_, radix);
-            auto buf = std::make_unique<char[]>(len); // NOLINT
-            mp_handle_error_(mp_rat_to_string(&a.num_, radix, buf.get(), len));
-            out << buf.get();
-        }
-        return out;
-    }
-
-private:
-    static void mp_handle_error_(mp_result res) {
-        if (res != MP_OK) {
-            if (res == MP_MEMORY) {
-                throw std::bad_alloc();
-            }
-            if (res == MP_RANGE || res == MP_TRUNC) {
-                throw std::range_error(mp_error_string(res));
-            }
-            if (res == MP_UNDEF) {
-                throw std::domain_error(mp_error_string(res));
-            }
-            if (res == MP_BADARG) {
-                throw std::invalid_argument(mp_error_string(res));
-            }
-            throw std::logic_error(mp_error_string(res));
-        }
-    }
-
-    mutable mpq_t num_;
-};
-
-*/
-
 
 #endif
