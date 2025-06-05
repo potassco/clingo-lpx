5180b42cada2c5f9a88abc9f666ef3a3653b7366
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-06-05 08:48:48 +0200

    prepare conda packages

---
+++ .github/conda/bld.bat
@@ -0,0 +1,12 @@
+cmake -S . -B build -G "Visual Studio 17 2022" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR="lib" ^
+    -DPython_ROOT_DIR="%PREFIX%" ^
+    -DPython_EXECUTABLE="%PYTHON%" ^
+    -DCLINGOLPX_MANAGE_RPATH=OFF ^
+    -DCLINGOLPX_BUILD_TESTS=ON ^
+    -DPYCLINGOLPX_INSTALL_DIR="%SP_DIR%"
+
+cmake --build build --config Release
+ctest --test-dir build -C Release
+cmake --build build --target install --config Release
