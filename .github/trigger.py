583eceb46c731e8d352834b06dc7248069b92ee2
Roland Kaminski <rkaminsk@users.noreply.github.com>
2025-11-28 15:46:35 +0100

    Cloudsmith (#27)

--- .github/trigger.py
+++ .github/trigger.py
@@ -15,6 +15,7 @@ TOKEN_FILE = os.path.expanduser("~/.tokens")
 WORKFLOW_ID_CONDA = "166422611"
 WORKFLOW_ID_PYPI = "190938747"
 WORKFLOW_ID_PPA = "190948560"
+WORKFLOW_ID_CLOUDSMITH = "211194935"
 
 
 def get_token():
@@ -107,6 +108,11 @@ def main():
             args.branch,
             {"build_number": args.build_number, "type": "stable"},
         )
+        dispatch_workflow(
+            WORKFLOW_ID_CLOUDSMITH,
+            args.branch,
+            {"build_number": args.build_number, "type": "stable"},
+        )
     elif args.command == "dev":
         dispatch_workflow(
             WORKFLOW_ID_CONDA,
@@ -120,6 +126,11 @@ def main():
         )
         dispatch_workflow(
             WORKFLOW_ID_PPA,
+            args.branch,
+            {"build_number": "auto", "type": "wip-20"},
+        )
+        dispatch_workflow(
+            WORKFLOW_ID_CLOUDSMITH,
             args.branch,
             {"build_number": "auto", "type": "wip-20"},
         )
